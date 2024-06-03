8c8722c7470acfd10bcd381aa1e49bd38baad7d5
“milliybro” <shohrux-rustamov@mail.ru>
2024-06-03 10:18:08 +0500

    removed container

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { ToastContainer } from "react-toastify";
+// import { ToastContainer } from "react-toastify";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
