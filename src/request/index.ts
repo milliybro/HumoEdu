b0f20c1b43e0075f94c287a753e8ca42626f07cb
“milliybro” <shohrux-rustamov@mail.ru>
2024-06-03 10:57:49 +0500

    removed container

--- src/request/index.ts
+++ src/request/index.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import { TOKEN } from "../constants";
 
 export const request = axios.create({
-  baseURL: "http://51.20.248.99:8000/api/v1",
+  baseURL: "http://13.60.86.61:8000/api/v1",
   timeout: 10000,
   headers: {
     Authorization: Cookies.get(TOKEN) ? "Bearer " + Cookies.get(TOKEN) : null
